895a9c724cad204123a20cd3ad17a4ab3b0266a9
laurenstolk <90372627+laurenstolk@users.noreply.github.com>
2022-01-19 08:26:22 -0700

    thanks tyler!

---
+++ StolkLaurenGradeCalculator/Models/GradeCalculatorModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StolkLaurenGradeCalculator.Models
+{
+    public class GradeCalculatorModel
+    {
+            [Required]
+            [Range(0, 100,
+            ErrorMessage = "Assignment grade must be between {1} and {2}.")]
+            public int assignment { get; set; }
+
+            [Required]
+            [Range(0, 100,
+            ErrorMessage = "Group Project grade must be between {1} and {2}.")]
+            public int groupProject { get; set; }
+
+            [Required]
+            [Range(0, 100,
+            ErrorMessage = "Quiz grade must be between {1} and {2}.")]
+            public int quiz { get; set; }
+
+            [Required]
+            [Range(0, 100,
+            ErrorMessage = "Exam grade must be between {1} and {2}.")]
+            public int exam { get; set; }
+
+            [Required]
+            [Range(0, 100,
+            ErrorMessage = "INTEX grade must be between {1} and {2}.")]
+            public int intex { get; set; }
+    }
+}
---
+++ StolkLaurenGradeCalculator/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,37 @@
+﻿
+// send an alert with final percentage and letter grade
+$("#btnSend").click(function () {
+    // calculate final percentage with weights
+    var finalPerc = ( ($("#assignment").val() * 0.55) +
+        ($("#groupProject").val() * 0.05) +
+        ($("#quiz").val() * 0.1) +
+        ($("#exam").val() * 0.2) +
+        ($("#intex").val() * 0.1)).toFixed(2)
+
+    // initialize letter grade variable
+    var letterGrade;
+
+    // determine letter grade
+    if (finalPerc >= 94) {
+        letterGrade = "A";
+    } else if (finalPerc >= 90) {
+        letterGrade = "A-";
+    } else if (finalPerc >= 87) {
+        letterGrade = "B+";
+    } else if (finalPerc >= 84) {
+        letterGrade = "B";
+    } else if (finalPerc >= 80) {
+        letterGrade = "B-";
+    } else if (finalPerc >= 77) {
+        letterGrade = "C+";
+    } else {
+        letterGrade = "E";
+    };
+
+    alert("Final Grade: " + finalPerc +
+        " (" + letterGrade + ")");
+});
+
+//test that this jquery page is hooked in correctly
+console.log("t4est");
+
